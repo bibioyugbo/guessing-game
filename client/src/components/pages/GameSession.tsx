94f996f50f3ee1293c287c52eb48817601ebaffc
bibi oyugbo <obehioyeoyugbo@gmail.com>
2025-09-02 10:05:21 +0100

    Game master setting screen

--- client/src/components/pages/GameSession.tsx
+++ client/src/components/pages/GameSession.tsx
@@ -11,9 +11,20 @@ export default function GameSession({isGameMaster}:GameSessionProps){
             <div className={"bg-amber-100 flex p-5 justify-center items-end h-screen"}>
                 {
                     isGameMaster?
-                        <div className={"flex w-[700px] flex-col"}>
-                            <input type={"text"} className={"bg-amber-400  p-8 rounded-md"} placeholder={"Enter your question"}/>
-                            <input type={"text"} className={"bg-amber-400 p-8 rounded-md"} placeholder={"Enter the answer to your question"}/>
+                        <div className={"flex  h-full gap-4 flex-col"}>
+                            <div className={"bg-amber-700 rounded-md text-white border-2 text-center p-5 w-auto"}>Hi GameMaster!</div>
+                           <div className={"justify-between h-full flex flex-col"}>
+                               <div className={"bg-amber-400 gap-3 p-4 justify-center items-center flex rounded-md"}>
+                                   <input type={"text"} className={"bg-white outline-none w-[650px] p-8 rounded-[12px]"} placeholder={"Enter your question"}/>
+                                   <button className={"bg-amber-700 active:scale-105 transition-transform cursor-pointer rounded-[10px] text-white max-h-[40px] p-2 "}>Set question</button>
+                               </div>
+                               <input type={"text"} className={"bg-amber-400 outline-none w-full rounded-br-3xl p-8 rounded-md"} placeholder={"Enter the answer to your question"}/>
+                           </div>
+                            <div className={"flex justify-end"}>
+                                <button className={"bg-amber-700 active:scale-105 max-w-[200px] transition-transform cursor-pointer rounded-[10px] text-white max-h-[40px] p-2 "}>Set answer</button>
+                            </div>
+
+
                         </div>:
                         <div className={"flex gap-60 flex-col"}>
                             <div className={"bg-amber-700  rounded-t-3xl rounded-br-3xl w-[700px] p-8 rounded-md"}>
